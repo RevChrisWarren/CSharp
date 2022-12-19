081ace1bd29543786eb3151cbcf0a6eff97ac7f6
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-12-18 20:44:50 -0600

    first app

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Sample
+{
+    static void Main()
+    {
+        System.Console.WriteLine("Hello!");
+        System.Console.ReadKey();
+    }
+}
 
