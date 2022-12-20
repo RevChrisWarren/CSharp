daf626b4804e02b7c75f5d59121fcd09ddd6ef38
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-12-19 22:07:46 -0600

    System console methods

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -2,7 +2,23 @@
 {
     static void Main()
     {
-        System.Console.WriteLine("Hello World!");
+        //Print messages line by line
+        System.Console.WriteLine("Welcome");
+        System.Console.WriteLine("to");
+        System.Console.WriteLine("C# Programming");
+
+        //Print messages on same line
+        System.Console.Write("Welcome ");
+        System.Console.Write("to ");
+        System.Console.Write("C# Programming");
+
+        //Wait for pressing a key on keyboard
+        System.Console.ReadKey();
+
+        //clear the screen
+        System.Console.Clear();
+
+        System.Console.WriteLine("Thank You!");
         System.Console.ReadKey();
     }
 }
