e0290baa63ed27e2be332de7b4db72fafb687399
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-12-23 11:08:28 -0600

    for while and other loops

---
+++ Assignment7/Assignment7/Program.cs
@@ -0,0 +1,32 @@
+﻿class ForLoop
+{
+    static void Main()
+    {
+        int i = 1;
+        while (i <= 10)
+        {
+            System.Console.Write(i + " ");
+            i++;
+        }
+        System.Console.WriteLine();
+
+        i = 1;
+        do
+            
+        { 
+            System.Console.Write(i + " ");
+            i++;
+        } while (i <= 10);
+
+        System.Console.WriteLine();
+
+        for (i = 1; i<= 10; i++)
+        {
+            System.Console.Write(i + " ");
+        }
+
+        System.Console.ReadKey();
+    }
+   
+}
+
