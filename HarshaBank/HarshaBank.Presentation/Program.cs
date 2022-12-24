95fd4cd6f083e1e2efea1fa8a5e6c5314730f309
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-12-24 15:42:23 -0600

    Menu built

--- HarshaBank/HarshaBank.Presentation/Program.cs
+++ HarshaBank/HarshaBank.Presentation/Program.cs
@@ -1,6 +1,6 @@
 ﻿class Program
 {
-static void Main()
+    static void Main()
     {
         //display title
         System.Console.WriteLine("************Harsha Bank************");
@@ -13,17 +13,53 @@ static void Main()
         System.Console.WriteLine("Username:");
         userName = System.Console.ReadLine();
 
-        if(userName != "")
+        if (userName != "")
         {
             //read password from keyboard only if userName is entered
             System.Console.WriteLine("Password:");
             password = System.Console.ReadLine();
         }
 
-        //chech userName and password
-        if(userName == "system" && password == "manager")
+        //check userName and password
+        if (userName == "system" && password == "manager")
         {
-            System.Console.WriteLine("TODO: Main Menu Here");
+            // declare variable to store menu choice
+            int mainMenuChoice = -1;
+            do
+            {
+                
+
+                //Show main menu
+                System.Console.WriteLine("\n:::Main Menu Here:::");
+                System.Console.WriteLine("1. Customers");
+                System.Console.WriteLine("2. Accounts");
+                System.Console.WriteLine("3. Funds Transfer");
+                System.Console.WriteLine("4. Funds Transfer Statement");
+                System.Console.WriteLine("5. Account Statement");
+                System.Console.WriteLine("0. Exit");
+
+                System.Console.Write("Enter Choice: ");
+                mainMenuChoice = int.Parse(System.Console.ReadLine());
+
+                switch (mainMenuChoice)
+                {
+                    case 1: //TODO: DISPLAY CUSTOMERS MENU
+                        break;
+                    case 2: //TODO: DISPLAY ACCOUNTS MENU
+                        break;
+                    case 3: //TODO: DISPLAY FUNDS TRANSFER MENU
+                        break;
+                    case 4: //TODO: DISPLAY FUNDS TRANSFER STATEMENT MENU
+                        break;
+                    case 5: //TODO: DISPLAY ACCOUNT STATEMENT MENU
+                        break;
+
+                }
+            } while (mainMenuChoice != 0);
+
+        }
+        else {
+            System.Console.WriteLine("Invalid username or password");
         }
 
         //about to exit
